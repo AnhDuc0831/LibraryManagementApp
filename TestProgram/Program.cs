8c981e89eb33094219a9acfb896a3cb1f0799eb6
AnhDuc0831 <chauanhduc3110@gmail.com>
2025-11-20 16:38:18 +0700

    Done cấu hình

---
+++ TestProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using DataAccsess;
+
+namespace TestProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = new DBLibraryManagementContext();
+            Console.WriteLine(db.Students.ToList()[0].FullName);
+        }
+    }
+}
